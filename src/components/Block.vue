ce08cb5b95f4af682603eaa09583745841db5bd5
mkholidkamali <mohamadkholid.kamali@gmail.com>
2022-09-22 15:14:05 +0700

    [New] Component & Block Statement

---
+++ src/components/Block.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="block" v-if="showBlock" @click="stopTimer">
+        click me
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+        }, this.delay)
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+        }
+    }
+}
+</script>
+
+<style>
+    .block {
+        width: 200px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: white;
+        text-align: center;
+        padding: 100px;
+        margin: 40px auto;
+    }
+</style>
