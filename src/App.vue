c709032f0b562ff1aaa83899d19d902acdc2e095
mkholidkamali <mohamadkholid.kamali@gmail.com>
2022-09-22 16:54:04 +0700

    [Not Completed] Result Component]

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 <template>
   <h1>Kk Reaction Timer</h1>
-  <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay"></Block>
+  <button class="play" @click="start" :disabled="isPlaying">PLAY</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"></Block>
+  <Result v-if="showResult" :result="score"></Result>
 </template>
 
 <script>
 import Block from "./components/Block.vue"
+import Result from "./components/Result.vue"
 
 export default {
   name: "App",
-  components: { Block },
+  components: { Block, Result },
   data() {
     return {
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResult: false
     }
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000,
       this.isPlaying = true
+      this.showResult = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResult = true
     }
   }
 }
@@ -33,5 +43,17 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+.play {
+  color: white;
+  background: teal;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: 0px;
+}
+.play:disabled {
+  background: rgba(0, 0, 0, 0.5);
+  color: rgba(0, 0, 0, 0.5);
+  z-index: -1;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1 class="results">Result : {{ result }}.</h1>
+</template>
+
+<script>
+export default {
+    props: ['result'],
+    data() {
+        return  {
+            rank: {
+                name: "Mohamad Kholid Kamali"
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    .results { 
+        text-align: center;
+        margin-top: 50px;
+    }
+</style>
